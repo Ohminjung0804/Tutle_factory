380ec9a6a3bdcf3e22bcb2396d5fa3b821e23c33
Jyuri <jeon1310@gmail.com>
2022-06-17 15:13:17 +0900

    스트레칭 데이터 생성

--- JS/turtlecheck.js
+++ JS/turtlecheck.js
@@ -2,10 +2,12 @@
 // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
 
 // the link to your model provided by Teachable Machine export panel
+console.log("init 실행시키기");
+
 const URL = "./my_model/";
 // import { edit_ease_turtle } from "../JS/turtles.js";
 let model, webcam, ctx, labelContainer, maxPredictions;
-let percent = 0;
+let percent = 0;    // 테스트 완화도 데이터(정수)
 let call_cnt  =0;   // 호출횟수
 // user_email = localStorage.getItem('key');
 // edit_ease_turtle(email, result); // 이메일과 결과 넘겨주기 -> result는 int형
@@ -43,6 +45,74 @@ async function init() {
     }
 
 }
+// 쿠키 설정
+function setCookie(key, value, expiredays) {
+    var todayDate = new Date();
+    todayDate.setDate(todayDate.getDate() + expiredays);
+    document.cookie = key + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+}
+// 쿠키 가져오기
+function getCookie(key) {
+    var result = null;
+    var cookie = document.cookie.split(';');
+    console.log(cookie);
+    cookie.some(function (item) {
+        // 공백을 제거
+        item = item.replace(' ', '');
+
+        var dic = item.split('=');
+
+        if (key === dic[0]) {
+            result = unescape(dic[1]);
+            return true; // break;
+        }
+    });
+    return result;
+}
+
+// 데이터 변경
+let edit_turtle = function (email, ease) {
+    console.log(email)
+    $(document).ready(function () {
+        $.ajax({
+            type: "PUT",
+            url: 'http://107.21.77.37/turtle/user?user_email=' + email,
+            dataType: "json",
+            accept: "application/json",
+            data: JSON.stringify({
+                "email": email,
+                "name": getCookie("name"),
+                "num": getCookie("num"),
+                "best": getCookie("best"),
+                "ease": ease
+            }),
+            //전달할 때 사용되는 파라미터 변수명
+            // 이 속성을 생략하면 callback 파라미터 변수명으로 전달된다.
+            success: function (data, textStatus, jqXHR) {
+                console.log('success');
+                console.log(data)
+                setCookie("email",data.email,100);
+                setCookie("name",data.name,100);
+                setCookie("num",data.num,100);
+                setCookie("ease",data.ease,100);
+                setCookie("best",data.best,100);
+                setCookie("created",data.created,100);
+                console.log(document.cookie);
+                // console.log(JSON.parse(data[0]));
+            },
+            complete: function (d) {
+                console.log('d')
+            },
+            error: function (xhr, textStatus, error) {
+                console.log(xhr.responseText);
+                console.log(textStatus);
+                console.log(error);
+            }
+        });
+    });
+}
+
+// 테스트 데이터 수정 
 
 // document.getElementById('startbtn').addEventListener('click', init);
 
@@ -52,6 +122,9 @@ async function loop(timestamp) {
     let t = window.requestAnimationFrame(loop);
     if(call_cnt>=80){
         window.cancelAnimationFrame(t);
+        let user_email = localStorage.getItem("key",''); // 이메일 가져오기
+        console.log(user_email);
+        edit_turtle(user_email,percent); // 테스트 데이터 저장부분 수정
     }
 }
 
@@ -67,28 +140,31 @@ async function predict() {
     call_cnt+=1;
     // var result = Number(prediction[0].probability.toFixed(2));
 
-    var result = Number(prediction[0].probability.toFixed(2));
+    // var result = Number(prediction[0].probability.toFixed(2));
 
-    setTimeout(function () {
-        if(result == 1.00){
+    // setTimeout(function () {
+    //     if(result == 1.00){
 
-            Object.freeze(labelContainer);
-            return;
-        } else{
-            labelContainer.innerHTML = "조금 떨어져서 우측을 바라보고 올바르게 서주세요.";
-            Object.freeze(labelContainer);
-            return;
-        }
-    }, 5000);
+    //         Object.freeze(labelContainer);
+    //         return;
+    //     } else{
+    //         labelContainer.innerHTML = "조금 떨어져서 우측을 바라보고 올바르게 서주세요.";
+    //         Object.freeze(labelContainer);
+    //         return;
+    //     }
+    // }, 5000);
 
     // 무슨 동작을 하는지 적힘
+    // console.log('완화도',Math.round(prediction[1].probability.toFixed(2)*100));
+    percent = Math.round(prediction[1].probability.toFixed(5)*1000);
+    console.log(percent);
+    console.log(prediction[1].probability.toFixed(5));
+    
     for (let i = 0; i < maxPredictions; i++) {
         const classPrediction =
             prediction[i].className + ": " + prediction[i].probability.toFixed(2);
         labelContainer.childNodes[i].innerHTML = classPrediction;
     }
-    console.log('완화도',Math.round(prediction[1].probability.toFixed(2)*100));
-    percent = Math.round(prediction[1].probability.toFixed(2));
 
     // Object.freeze(result);
     // labelContainer.innerHTML = result;
--- traning_1/JS/Traning_1.js
+++ traning_1/JS/Traning_1.js
@@ -5,6 +5,52 @@
 const URL = "./my_model/";
 // import { create_cure } from "../../JS/tranings.js";
 let model, webcam, ctx, labelContainer, maxPredictions;
+let creatchk = true;
+
+// 스트레칭 생성
+let create_cure = function (stretchingNum, cnt) {
+    var data1 = null
+    $(document).ready(function () {
+        $.ajax({
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({
+                "stretch": stretchingNum,
+                "status": cnt,
+                "user_email": localStorage.getItem('key')
+            }),
+            url: 'http://107.21.77.37/cure/',
+            dataType: "json",
+            accept: "application/json",
+            //전달할 때 사용되는 파라미터 변수명
+            // 이 속성을 생략하면 callback 파라미터 변수명으로 전달된다.
+            success: function (data, textStatus, jqXHR) {
+                console.log('success');
+                console.log(data);
+                var data1 = {
+                    id: data[0].id,
+                    craeted: data[0].created,
+                    stretch: data[0].stretch,
+                    status: data[0].status,
+                    user_email: data[0].user_email,
+                }
+                // console.log(JSON.parse(data[0]));
+                console.log(data1)
+                // console.log(JSON.parse(data[0]));
+                console.log(data1)
+                $('.result').html('dfdjflks' + data1.email);
+            },
+            complete: function (d) {
+                console.log('d')
+            },
+            error: function (xhr, textStatus, error) {
+                console.log(xhr.responseText);
+                console.log(textStatus);
+                console.log(error);
+            }
+        });
+    });
+}
 
 // 스트레칭 번호
 const stretchingNum = 1;
@@ -131,9 +177,14 @@ async function predict() {
         
     });
 
-    if (cnt == 10) {
+    if (cnt == 3) {
         // 스트레칭 생성
         // 전유리 이거 넣으면 에러 남
+        if(creatchk===true){
+            create_cure(stretchingNum,cnt);
+            creatchk= false;
+        }
+
         // create_cure(stretchingNum, cnt);
     }
 
